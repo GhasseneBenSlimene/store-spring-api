baccc66c47729513e39f3d8f3826bcb1fe7497a3
GhasseneBenSlimene <ghassenebenslimene@gmail.com>
2025-09-16 13:38:47 +0000

    Build API foundation

---
+++ src/main/java/com/codewithmosh/store/controllers/MessageController.java
@@ -0,0 +1,13 @@
+package com.ghassenebenslimene.store.controllers;
+
+import com.ghassenebenslimene.store.entities.Message;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+    @RequestMapping("/hello")
+    public Message sayHello() {
+        return new Message( "Hello World!");
+    }
+}
