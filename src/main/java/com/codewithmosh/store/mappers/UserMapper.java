d830bea4e90ebfc38ee50d57dee5626570e40999
GhasseneBenSlimene <ghassenebenslimene@gmail.com>
2025-10-07 13:21:58 +0000

    Updating resources

--- src/main/java/com/codewithmosh/store/mappers/UserMapper.java
+++ src/main/java/com/codewithmosh/store/mappers/UserMapper.java
@@ -1,12 +1,15 @@
 package com.ghassenebenslimene.store.mappers;
 
 import com.ghassenebenslimene.store.dtos.RegisterUserRequest;
+import com.ghassenebenslimene.store.dtos.UpdateUserRequest;
 import com.ghassenebenslimene.store.dtos.UserDto;
 import com.ghassenebenslimene.store.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserDto toDto(User user);
     User toEntity(RegisterUserRequest request);
+    void update(UpdateUserRequest request, @MappingTarget User user);
 }
