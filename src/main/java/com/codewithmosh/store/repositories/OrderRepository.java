ed3b73f7a90d82a5a88a643ea2d945a0ece317b2
GhasseneBenSlimene <ghassenebenslimene@gmail.com>
2025-11-14 11:49:06 +0000

    Checking out

---
+++ src/main/java/com/codewithmosh/store/repositories/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.ghassenebenslimene.store.repositories;
+
+import com.ghassenebenslimene.store.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+  }
