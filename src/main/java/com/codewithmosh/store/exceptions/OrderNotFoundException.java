2f42f689549bb346ef8e323f1446007cec075dc4
GhasseneBenSlimene <ghassenebenslimene@gmail.com>
2025-11-16 09:12:28 +0000

    Getting a single order

---
+++ src/main/java/com/codewithmosh/store/exceptions/OrderNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ghassenebenslimene.store.exceptions;
+
+public class OrderNotFoundException extends RuntimeException {
+    public OrderNotFoundException() {
+        super("Order not found");
+    }
+}
