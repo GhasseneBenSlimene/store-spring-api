2cc86b0516a867a588e971acfd08744cdd6e37fc
GhasseneBenSlimene <ghassenebenslimene@gmail.com>
2025-11-17 15:13:58 +0000

    Decoupling from Stripe

---
+++ src/main/java/com/codewithmosh/store/services/CheckoutSession.java
@@ -0,0 +1,10 @@
+package com.ghassenebenslimene.store.services;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CheckoutSession {
+    private String checkoutUrl;
+}
