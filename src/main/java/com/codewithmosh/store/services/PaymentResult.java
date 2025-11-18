6950bbf23b3cdc0d0cc71c6e3d55fabb3e2f52ef
GhasseneBenSlimene <ghassenebenslimene@gmail.com>
2025-11-18 10:25:20 +0000

    Refactoring the webhook logic

---
+++ src/main/java/com/codewithmosh/store/services/PaymentResult.java
@@ -0,0 +1,12 @@
+package com.ghassenebenslimene.store.services;
+
+import com.ghassenebenslimene.store.entities.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class PaymentResult {
+    private Long orderId;
+    private PaymentStatus paymentStatus;
+}
