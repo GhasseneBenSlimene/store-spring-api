36d7bccf7774ae8cd3efa0ca304db1f376cb8d04
Ghassene Ben Slimene <123106762+GhasseneBenSlimene@users.noreply.github.com>
2025-11-26 12:15:48 +0100

    move db and jwt secrets to .env

--- src/main/java/com/ghassene/store/services/JwtService.java
+++ src/main/java/com/ghassene/store/services/JwtService.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -11,13 +12,16 @@ import java.util.Map;
 
 @Service
 public class JwtService {
+    @Value("${spring.jwt.secret}")
+    private String secret;
+
     public String generateToken(String subject) {
         final long tokenExpiration = 86400; // 1 day
         return Jwts.builder()
                 .setSubject(subject)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
-                .signWith(Keys.hmacShaKeyFor("secret".getBytes()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
                 .compact();
     }
 }
