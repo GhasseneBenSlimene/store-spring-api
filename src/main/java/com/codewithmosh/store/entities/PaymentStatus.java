a80ee7f72a6ea134a072f0584cc8279acf647961
GhasseneBenSlimene <ghassenebenslimene@gmail.com>
2026-01-01 15:06:17 +0100

    Refactoring the webhook logic

--- src/main/java/com/codewithmosh/store/entities/PaymentStatus.java
+++ src/main/java/com/codewithmosh/store/entities/PaymentStatus.java
@@ -1,6 +1,6 @@
 package com.ghassenebenslimene.store.entities;
 
-public enum OrderStatus {
+public enum PaymentStatus {
     PENDING,
     PAID,
     FAILED,
--- src/main/java/com/codewithmosh/store/services/PaymentGateway.java
+++ src/main/java/com/codewithmosh/store/services/PaymentGateway.java
@@ -2,6 +2,9 @@ package com.ghassenebenslimene.store.services;
 
 import com.ghassenebenslimene.store.entities.Order;
 
+import java.util.Optional;
+
 public interface PaymentGateway {
     CheckoutSession createCheckoutSession(Order order);
+    Optional<PaymentResult> parseWebhookRequest(WebhookRequest request);
 }
