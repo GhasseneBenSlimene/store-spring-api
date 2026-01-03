a5a46305f729ec53bc3f3ecddbb8c0e0617c205b
GhasseneBenSlimene <ghassenebenslimene@gmail.com>
2026-01-03 14:31:24 +0000

    Modularizing security rules by feature

---
+++ src/main/java/com/codewithmosh/store/admin/AdminSecurityRules.java
@@ -0,0 +1,15 @@
+package com.ghassenebenslimene.store.admin;
+
+import com.ghassenebenslimene.store.common.SecurityRules;
+import com.ghassenebenslimene.store.users.Role;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminSecurityRules implements SecurityRules {
+    @Override
+    public void configure(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+        registry.requestMatchers("/admin/**").hasRole(Role.ADMIN.name());
+    }
+}
